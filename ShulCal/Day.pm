db789acb690dd2ff148dd0d5dffc7b3886a193b7
sevrinsky <sevrinsky@gmail.com>
2019-03-06 17:12:34 +0200

    Convert Shushan Purim to standard handling for youth minyan

--- ShulCal/Day.pm
+++ ShulCal/Day.pm
@@ -654,10 +654,12 @@ sub get_times {
           }
       }
       my $youth_minyan_time = '8:45';
-      if ($sof_zman_kriat_shma lt ShulCal::Time->new('8:45')) {
+      if ($sof_zman_kriat_shma lt ShulCal::Time->new('8:55')) {
           $youth_minyan_time = '8:30';
       }
-      $davening_times{"shacharit"} .= ", $youth_minyan_time";
+      if ($self->dow_0 != 5 || $youth_minyan_time ne '8:30') {
+          $davening_times{"shacharit"} .= ", $youth_minyan_time";
+      }
   }
 
   if (!$self->is_shabbat) {
